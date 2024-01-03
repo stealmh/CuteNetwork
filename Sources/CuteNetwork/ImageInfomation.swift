f1ac1fd346209ff34badf475f48bf632b0f1b9cf
mino <kmh922@naver.com>
2024-01-03 16:09:16 +0900

    feat: Image Infomation

---
+++ Sources/CuteNetwork/ImageInfomation.swift
@@ -0,0 +1,14 @@
+//
+//  ImageInfomation.swift
+//  
+//
+//  Created by mino on 2024/01/03.
+//
+
+import UIKit
+
+public struct ImageInfomation {
+    let imageName: String
+    let imageType: String
+    let image: UIImage
+}
